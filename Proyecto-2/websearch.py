3c3ae34300b5433f1b3272e9651e37e865183315
CarlosMGomezdelasHeras <92698313+CarlosMGomezdelasHeras@users.noreply.github.com>
2022-11-15 10:51:05 +0100

    Add files via upload

--- Proyecto-2/websearch.py
+++ Proyecto-2/websearch.py
@@ -10,4 +10,33 @@ print(len(excelL))
 for i in excelL:
     print(i[0])
     r = requests.get(i[0])
-    print(r)
+    imdb= BeautifulSoup(r.text, 'html.parser')
+    req = requests.get(i[0]+'/synopsis?ref_=tt_stry_pl')
+    sinopsis_url = BeautifulSoup(req.text, 'html.parser')
+    urls = imdb.select('a[href]')
+    urls_list = [urls.string for urls in urls]
+    title = imdb.select('h1.sc-b73cd867-0')[0].text.strip()
+    director = imdb.select('a.ipc-metadata-list-item__list-content-item--link')[0].text.strip()
+    guionist = imdb.select('a.ipc-metadata-list-item__list-content-item--link')[1].text.strip()
+    actor = imdb.select('a.sc-bfec09a1-1')
+    genre = []
+    genre_list = []
+    actor_list = []
+    sinopsis = sinopsis_url.find("li", {"id": "synopsis-py5253095"})
+    #sinopsis = sinopsis_url.select('li.ipl-zebra-list__item.synopsis-py5253095')
+    genre = imdb.select('span.ipc-chip__text')
+    score = imdb.select('span.sc-7ab21ed2-1')[0].text.strip()
+    for j in genre:
+        genre_list.append(j.text.strip())
+    for j in actor:
+        actor_list.append(j.text.strip())
+    print(title)
+    print(genre_list)
+    print(actor_list)
+    print(director)
+    print(guionist)
+    print(score)
+    print(type(sinopsis))
+    #print(urls_list)
+    
+
