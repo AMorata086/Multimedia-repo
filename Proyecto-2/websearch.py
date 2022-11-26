a8cf557aca1428d4fcd0b6dcf1076277497085f2
CarlosMGomezdelasHeras <92698313+CarlosMGomezdelasHeras@users.noreply.github.com>
2022-11-26 14:01:44 +0100

    crawler complete

--- Proyecto-2/websearch.py
+++ Proyecto-2/websearch.py
@@ -7,38 +7,36 @@ import pandas as pd
 usecolsA = [1]
 required_df = pd.read_excel('imdbtitles.xlsx', usecols = usecolsA, skiprows = 1 )
 excelL = required_df.values.tolist()
+output = open('movies.json', 'a', encoding='utf8')
+dictList = []
 #print(len(required_df))
 #print(len(excelL))
 for i in excelL:
     dictMovie = {}
     print(i[0])
-    r = requests.get(i[0]+'/reference')
-    print(r)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102'}
+    r = requests.get(i[0],headers=headers)
+    r.encoding
+    #r = requests.get(i[0]+'/reference')
     imdb= BeautifulSoup(r.text, 'html.parser')
     req = requests.get(i[0]+'/synopsis?ref_=tt_stry_pl')
-    print(req)
     sinopsis_url = BeautifulSoup(req.text, 'html.parser')
     urls = imdb.select('a[href]')
     urls_list = [urls.string for urls in urls]
-    #title = imdb.select('h1.sc-b73cd867-0')[0].text.strip()
-    title = imdb.select('h3')[0].text.strip()
-    titulo = title.split("  ")
-    titulo = titulo[0].strip()
-    print(titulo)
-    year= imdb.select('span.titlereference-title-year')[0].text.strip()
-    year = year.replace('(','')
-    year = year.replace(')','')
-    print(year)
-    #director = imdb.select('a.ipc-metadata-list-item__list-content-item--link')[0].text.strip()
-    director = imdb.select('li.ipl-inline-list__item').text.strip()
-    print(director)
+    title = imdb.select('h1.sc-b73cd867-0')[0].text.strip()
+    year = imdb.select('span.sc-8c396aa2-2')[0].text.strip()
+    director = imdb.select('a.ipc-metadata-list-item__list-content-item--link')[0].text.strip()
     guionist = imdb.select('a.ipc-metadata-list-item__list-content-item--link')[1].text.strip()
     actor = imdb.select('a.sc-bfec09a1-1')
     genre = []
     genre_list = []
     actor_list = []
-    sinopsis = sinopsis_url.find("li", {"id": "synopsis-py5253095"})
-    #sinopsis = sinopsis_url.select('li.ipl-zebra-list__item.synopsis-py5253095')
+    summary = imdb.select('span.sc-16ede01-2')[0].text.strip()
+    max = len(sinopsis_url.select('li.ipl-zebra-list__item'))
+    sinopsis = sinopsis_url.select('li.ipl-zebra-list__item')[max-1].text.strip()
+    if("It looks like we don't have a Synopsis" in sinopsis):
+        sinopsis = ""
+    summary = str(summary).replace("Read all","")
     genre = imdb.select('span.ipc-chip__text')
     score = imdb.select('span.sc-7ab21ed2-1')[0].text.strip()
     for j in genre:
@@ -46,19 +44,15 @@ for i in excelL:
     for j in actor:
         actor_list.append(j.text.strip())
     dictMovie["title"] = title
+    dictMovie["year"] = year
     dictMovie["genres"] = genre_list
     dictMovie["actors"] = actor_list
     dictMovie["director"] = director
     dictMovie["guionist"] = guionist
     dictMovie["score"] = score
-    #print(title)
-    #print(genre_list)
-    #print(actor_list)
-    #print(director)
-    #print(guionist)
-    #print(score)
-    #print(type(sinopsis))
-    #print(urls_list)
-    print(dictMovie)
+    dictMovie["summary"] = summary
+    dictMovie["sinopsis"] = sinopsis
+    dictList.append(dictMovie)
+json.dump(dictList, output, ensure_ascii=False)
+print(output)
     
-
